3db6a51e511e82738271add43c608f7307a13e68
Tag0Mag0 <jem.hilton@gmail.com>
2014-08-11 12:06:28 -0500

    created user seed
    created welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
+  get 'home' => 'home#index'
+  root 'home#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
