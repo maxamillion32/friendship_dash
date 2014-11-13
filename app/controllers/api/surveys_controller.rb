b217b602ba6dcdd4359372ffb0868fd838e1d48c
Tag0Mag0 <jem.hilton@gmail.com>
2014-11-13 13:28:00 -0600

    fixed typo in surveys controller api

--- app/controllers/api/surveys_controller.rb
+++ app/controllers/api/surveys_controller.rb
@@ -1,5 +1,5 @@
 module Api
-  class UsersController < ApplicationController
+  class SurveysController < ApplicationController
     skip_before_action :authenticate_user!
     skip_before_filter  :verify_authenticity_token
     respond_to :json
