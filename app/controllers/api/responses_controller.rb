928aacd94a9b4057e6cd145c980e9a2b7f0c3735
Tag0Mag0 <jem.hilton@gmail.com>
2014-11-14 14:26:41 -0600

    updated params again to try to fix response import

--- app/controllers/api/responses_controller.rb
+++ app/controllers/api/responses_controller.rb
@@ -49,7 +49,7 @@ module Api
     private
 
     def response_params
-      params.permit(response: {}, responses: [ :guid, :participant_id, :user_id, :timestamp, responses: {}]).require(:responses)
+      params.permit(responses: {}, response: [ :guid, :participant_id, :user_id, :timestamp, responses: {}]).require(:responses)
     end
   end
 end
