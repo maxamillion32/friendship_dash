f4d0b6659829a6897e8fdad30112b0111c87a3ca
Tag0Mag0 <jem.hilton@gmail.com>
2014-11-26 12:00:51 -0600

    fixed syntax to appease the cops

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ActiveRecord::Base
-    has_many :survey_collections, -> {order "position"}, dependent: :destroy
+    has_many :survey_collections, -> { order "position" }, dependent: :destroy
     has_many :surveys, through: :survey_collections
 
     before_validation :generate_guid,  on: :create
